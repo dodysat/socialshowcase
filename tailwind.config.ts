ae383eccc09c73172430810debc7e65041a4fb22
Dody Satria <dody.satria13@gmail.com>
2025-01-01 18:52:48 +1100

    add daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import daisyui from "daisyui"
 
 export default {
   content: [
@@ -14,5 +15,8 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+} satisfies Config
